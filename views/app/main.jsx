200e4131697cf68234813d682fe0fa77ee3e589c
Peter N Tange <pt@tutee.dk>
2017-04-22 20:18:06 +0200

    Main Nav and css environment setup

--- views/app/main.jsx
+++ views/app/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
+require("./css/style.css");
 
 
 class Maincontainer extends React.Component {
@@ -13,9 +13,35 @@ class Maincontainer extends React.Component {
 
   render() {
     return (
-        <div>
-            <App />
+      <div>
+        <nav>
+        <div className="floatright">
+          <ul className="user">
+            <li>
+              <a href="#"><h1><i className="icon-user"></i>UserName</h1></a>
+            </li>
+            <li className="expand_toggle">
+              <a href="#"><h1>-</h1></a>
+            </li>
+          </ul>
+          <ul className="notifications">
+            <li >
+              <a href="#"><h1><i className="icon-user"></i>NodeName</h1></a>
+            </li>
+          </ul>
         </div>
+        <ul>
+          <li>
+            <img src="./img/spaceit_logo.svg"  alt="spaceit"/>
+          </li>
+          <li>
+            <a className="nav_logo" href="#"><h1><i className="icon-spaceit_logo"></i>SPACE<b>IT</b></h1></a>
+          </li>
+        </ul>
+
+
+        </nav>
+      </div>
 
     );
   }
