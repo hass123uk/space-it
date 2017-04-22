9d326e35f67c9d4ed33baec6e7629f300e67d02f
Peter N Tange <pt@tutee.dk>
2017-04-22 23:18:47 +0200

    Styled sidenav and user side panel plus added user stock images

--- views/app/main.jsx
+++ views/app/main.jsx
@@ -14,6 +14,7 @@ class Maincontainer extends React.Component {
   render() {
     return (
       <div>
+      {/*Main Nav bar*********************************************************/}
         <nav>
           <div className="floatright">
             <ul className="user">
@@ -40,8 +41,59 @@ class Maincontainer extends React.Component {
           </ul>
         </nav>
         <div className="navpush"></div>
-
-        
+        <div className="sidebar">
+          <div className="head">
+            <div className="node_op">
+              <div className="img_con"></div><p>Node Creator:</p>
+              <h3>NameOfCreator_OP</h3>
+            </div>
+            <ul className="node_stats">
+              <li>O* 2</li>
+              <li>O* 4</li>
+              <li>O* 6</li>
+            </ul>
+          </div>
+          <div className="node_info">
+            <h1>Name of Node</h1>
+            <p>
+              NodeDescription Gravida in tincidunt. Lectus neque donec consectetuer.
+              Hac wisi, quis nam, nulla lacinia vel, nisl sit, lorem platea dui massa arcu litora doloribus.
+            </p>
+          </div>
+        </div>
+        {/*User side panel*********************************************************/}
+        <main>
+          <div className="usersidepanel">
+            <ul>
+              <li className="head">
+                <div className="inner">
+                  MEMBERS
+                </div>
+              </li>
+              <li>
+                <div className="inner">
+                  <div className="img_con"></div><h3>UserName_1</h3>
+                </div>
+              </li>
+              <li>
+                <div className="inner">
+                  <div className="img_con"></div><h3>UserName_2</h3>
+                </div>
+              </li>
+              <li>
+                <div className="inner">
+                  <div className="img_con"></div><h3>UserName_3</h3>
+                </div>
+              </li>
+              <li>
+                <div className="inner">
+                  <div className="img_con"></div><h3>UserName_4</h3>
+                </div>
+              </li>
+            </ul>
+          </div>
+        {/*Sidebar*********************************************************/}
+        </main>
       </div>
     );
   }
